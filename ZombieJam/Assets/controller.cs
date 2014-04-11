998a6a496b8bb21422f193f4a2ca5f63861b39c9
kevinortegren <kevinortegren@gmail.com>
2014-04-11 22:29:40 +0200

    controller script updated

--- ZombieJam/Assets/controller.cs
+++ ZombieJam/Assets/controller.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class controller : MonoBehaviour {
 
+	public int JoyStickNum;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,12 +12,39 @@ public class controller : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		Vector3 deltaPos = new Vector3(0,0,0);
+
+		switch (JoyStickNum) {
+			case 1:
+			{
+				deltaPos = new Vector3 (Input.GetAxis ("HorizontalLeft1"), Input.GetAxis ("VerticalLeft1"), 0 );
+				break;
+			}
+			case 2:
+			{
+				deltaPos = new Vector3 (Input.GetAxis ("HorizontalLeft2"), Input.GetAxis ("VerticalLeft2"), 0 );
+				break;
+			}
+			case 3:
+			{
+				deltaPos = new Vector3 (Input.GetAxis ("HorizontalLeft3"), Input.GetAxis ("VerticalLeft3"), 0 );
+				break;
+			}
+			case 4:
+			{
+				deltaPos = new Vector3 (Input.GetAxis ("HorizontalLeft4"), Input.GetAxis ("VerticalLeft4"), 0 );
+				break;
+			}
+			default:
+				break;
+
+		}
+
 		if (Input.GetButtonDown ("Jump")) 
 		{
 			print("button Working");		
 		}
-
-		Vector3 deltaPos = new Vector3 (Input.GetAxis ("HorizontalLeft"), Input.GetAxis ("VerticalLeft"), 0 );
 
 		GetComponent<CharacterController> ().Move (deltaPos); 
 
