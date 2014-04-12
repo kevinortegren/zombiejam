f876e9df775efe59e18e8595f0aed622fa3dc212
Huxs <dc.jarle@hotmail.com>
2014-04-12 20:59:19 +0200

    Adds trap prefab

---
+++ ZombieJam/Assets/SpawnEnemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnemy : MonoBehaviour {
+
+    public GameObject enemyType;
+    public int count;
+
+    public void Trigger()
+    {
+        print("asd");
+        for (int i = 0; i < count; i++)
+        {
+            Instantiate(enemyType, transform.position, Quaternion.identity);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
