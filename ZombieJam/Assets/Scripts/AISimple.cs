2c8e793c077215b2c7a9cda0aa98eea707401627
Huxs <dc.jarle@hotmail.com>
2014-04-12 14:14:59 +0200

    Begin ai.

---
+++ ZombieJam/Assets/Scripts/AISimple.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AISimple : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach(GameObject p in players)
+        {
+            Vector3 dir = transform.position - p.transform.position;
+            print("testing player");
+
+            if (Physics.Raycast(transform.position, dir))
+            {
+                print("hit!");
+            }
+            
+        }
+
+        
+
+
+	}
+}
