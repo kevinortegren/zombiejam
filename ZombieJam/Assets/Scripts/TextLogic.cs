eb255276f6720c069e92763348b324339be4dbce
kevinortegren <kevinortegren@gmail.com>
2014-04-12 03:21:30 +0200

    Added menu scene with basic interaction

---
+++ ZombieJam/Assets/Scripts/TextLogic.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextLogic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseEnter(){
+		renderer.material.color = Color.red;
+	}
+
+	void OnMouseExit()
+	{
+		renderer.material.color = Color.white;
+	}
+
+	void OnMouseUp()
+	{
+		Application.LoadLevel (1);
+	}
+}
